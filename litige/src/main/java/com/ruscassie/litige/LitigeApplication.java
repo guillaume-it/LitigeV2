46b44d662467fa3ec70ffe23f37af8865e53b50b
Whazaby <Whazaby@users.noreply.github.com>
2019-10-20 11:03:29 +0200

    first commit

---
+++ litige/src/main/java/com/ruscassie/litige/LitigeApplication.java
@@ -0,0 +1,13 @@
+package com.ruscassie.litige;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitigeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitigeApplication.class, args);
+	}
+
+}
---
+++ litige/src/test/java/com/ruscassie/litige/LitigeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruscassie.litige;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LitigeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
