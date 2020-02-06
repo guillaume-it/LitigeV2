eff33fed547755093856d0c4ecd873ea00447030
Whazaby <guillaume-github@outlook.fr>
2020-02-06 20:58:42 +0100

    upgrade spring boot

--- litige-authorization/src/main/java/com/ruscassie/litige/configuration/AuthorizationServerConfigurerAdapter.java
+++ litige-authorization/src/main/java/com/ruscassie/litige/configuration/AuthorizationServerConfigurerAdapter.java
@@ -23,7 +23,6 @@ import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import com.ruscassie.litige.entity.User;
 
 import javax.sql.DataSource;
