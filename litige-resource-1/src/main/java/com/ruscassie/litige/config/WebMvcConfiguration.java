b733af7a94d0ff0e9a85d0807da3a3dcee9fe5f9
Whazaby <guillaume-github@outlook.fr>
2020-02-11 13:11:58 +0100

    clean

--- litige-resource-1/src/main/java/com/ruscassie/litige/config/WebMvcConfiguration.java
+++ litige-resource-1/src/main/java/com/ruscassie/litige/config/WebMvcConfiguration.java
@@ -1,29 +1,14 @@
 package com.ruscassie.litige.config;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.MethodParameter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.support.WebDataBinderFactory;
-import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.ruscassie.litige.dto.CustomPrincipal;
 
 @Configuration
 @EnableWebSecurity
 public class WebMvcConfiguration implements WebMvcConfigurer {
-
-	@Override
-	public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> argumentResolvers) {
-		argumentResolvers.add(currentUserHandlerMethodArgumentResolver());
-	}
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
@@ -31,26 +16,6 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 			@Override
 			public void addCorsMappings(final CorsRegistry registry) {
 				registry.addMapping("/**").allowedMethods("PUT", "DELETE", "POST", "GET", "PATCH").allowedOrigins("*");
-			}
-		};
-	}
-
-	@Bean
-	public HandlerMethodArgumentResolver currentUserHandlerMethodArgumentResolver() {
-		return new HandlerMethodArgumentResolver() {
-			@Override
-			public Object resolveArgument(final MethodParameter parameter, final ModelAndViewContainer mavContainer,
-					final NativeWebRequest webRequest, final WebDataBinderFactory binderFactory) throws Exception {
-				try {
-					return SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-				} catch (final Exception e) {
-					return null;
-				}
-			}
-
-			@Override
-			public boolean supportsParameter(final MethodParameter parameter) {
-				return parameter.getParameterType().equals(CustomPrincipal.class);
 			}
 		};
 	}
