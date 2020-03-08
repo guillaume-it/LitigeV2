cedd4083cacdd8f45c36f0812336ac4a31a47c26
Whazaby <guillaume-github@outlook.fr>
2020-03-08 17:33:28 +0100

    configuration a junit with mocks

--- litige-resource-1/src/test/java/com/ruscassie/litige/service/ClaimServiceMock.java
+++ litige-resource-1/src/test/java/com/ruscassie/litige/service/ClaimServiceMock.java
@@ -5,10 +5,11 @@ import com.ruscassie.litige.entity.Dossier;
 import com.ruscassie.litige.repository.ClaimRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -21,21 +22,19 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Fail.fail;
- import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.lenient;
 
-@SpringBootTest
-@ActiveProfiles("test")
-public class ClaimServiceTest {
+@ExtendWith(MockitoExtension.class)
+public class ClaimServiceMock {
 
 	@InjectMocks
 	private ClaimService claimService;
 
 	@Mock
 	private ClaimRepository claimRepository;
-//	@Autowired
-//	private StorageService storageService;
-//
+
 	@BeforeEach
 	public void setUp() {
 //        MockitoAnnotations.initMocks(this);
@@ -67,8 +66,8 @@ public class ClaimServiceTest {
 //		final Page<com.ruscassie.litige.entity.Claim> pageClaim = Page.empty(PageRequest.of(0, 3, Sort.by("id")));
 		// pageClaim.getContent().add(0, claim.get());
 
-		Mockito.when(claimRepository.findById(1L)).thenReturn(claim);
-		Mockito.when(claimRepository.findAll(Mockito.any(org.springframework.data.domain.Pageable.class)))
+		lenient().when(claimRepository.findById(1L)).thenReturn(claim);
+		lenient().when(claimRepository.findAll(Mockito.any(org.springframework.data.domain.Pageable.class)))
 				.thenReturn(page);
 
 	}
@@ -78,7 +77,7 @@ public class ClaimServiceTest {
 		try {
 			claimService.delete(1L);
 
-		} catch (final java.lang.IllegalArgumentException e) {
+		} catch (final IllegalArgumentException e) {
 			fail("Should not have thrown any exception");
 		}
 	}
