96c20dc864e37375a7b207ae5f693880bc3bdc6c
Guillaume <guillaume-other@outlook.fr>
2020-03-28 21:59:20 +0100

    fix angular build

--- litige-front/src/app/public/public.module.ts
+++ litige-front/src/app/public/public.module.ts
@@ -3,9 +3,10 @@ import { AddressComponent } from './signin-claimant/address/address.component';
 
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { SigninClaimantComponent } from './signin-claimant/signin-claimant.component';
 
 @NgModule({
-  declarations: [AddressComponent, ClaimCreateComponent],
+  declarations: [AddressComponent, ClaimCreateComponent, SigninClaimantComponent],
   imports: [SharedModule]
 })
 export class PublicModule {}
